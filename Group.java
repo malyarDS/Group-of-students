64d52db9d83635af5a1e8acd6c520e53ef851765
malyarDS <malyar.kr@gmail.com>
2017-10-17 23:02:48 +0300

    sixth commit

--- Group.java
+++ Group.java
@@ -77,7 +77,7 @@ public class Group implements Voencom {
     }
 
 
-    public  Student[] sortingGroup() {
+    public Student[] sortingGroup() {
         for (int i = 0; i < quantityStudent - 1
                 && i + 1 < quantityStudent; i++) {
             String sur1 = group[i].getSurname();
@@ -87,13 +87,15 @@ public class Group implements Voencom {
                 group[i] = group[i++];
                 group[i++] = temp;
             }
-        }return group;
+        }
+        return group;
     }
 
     public Student searchStudent(String surName) {
         for (int i = 0; i < quantityStudent; i++) {
             if (group[i] != null &&
                     group[i].getSurname() == surName) {
+                System.out.println("Seeking student № " + (i + 1) + " " + group[i]);
                 return group[i];
             }
         }
@@ -104,61 +106,63 @@ public class Group implements Voencom {
     @Override
     public Student[] militaryAge() {
         Student[] milAge = new Student[1];
-        Student[] temp=new Student[1];
+        Student[] temp = new Student[1];
         Student[] temp2 = new Student[1];
-        Student[]potentialConscript=new Student[1];
-        int quantityPotentialConscript=0;
-        int quantityMilitary=0;
-        for (int i = 0;i<quantityStudent-1;i++) {
+        Student[] potentialConscript = new Student[1];
+        int quantityPotentialConscript = 0;
+        int quantityMilitary = 0;
+        for (int i = 0; i < quantityStudent - 1; i++) {
             System.out.println("\n" +
                     "These students must undergo a " +
                     "medical commission");
             if (group[i] != null && group[i].getAge() > 16 &&
                     group[i].getSex().equals("men")) {
-                potentialConscript[quantityMilitary]=group[i];
+                potentialConscript[quantityMilitary] = group[i];
                 System.out.println(group[i]);
                 quantityPotentialConscript++;
                 System.arraycopy(potentialConscript
-                        ,0,temp2,0,quantityPotentialConscript);
-                potentialConscript=temp2;
-            }return potentialConscript;
+                        , 0, temp2, 0, quantityPotentialConscript);
+                potentialConscript = temp2;
+            }
+            return potentialConscript;
         }
-        for (int i = 0;i<quantityStudent-1;i++) {
+        for (int i = 0; i < quantityStudent - 1; i++) {
             if (group[i] != null && group[i].getAge() > 17 &&
-                    group[i].getAge()<27&&
+                    group[i].getAge() < 27 &&
                     group[i].getSex().equals("men")) {
-                     milAge[quantityMilitary]=group[i];
-                     quantityMilitary++;
-                     System.arraycopy(milAge,0,temp,0,quantityMilitary);
-                     milAge=temp;
+                milAge[quantityMilitary] = group[i];
+                quantityMilitary++;
+                System.arraycopy(milAge, 0, temp, 0, quantityMilitary);
+                milAge = temp;
             }
         }
         System.out.println("These students age of conscription");
         System.out.println(milAge);
         return milAge;
     }
+
     public void interactivAddStudent() throws InteractivAddExeption, GroupIsOvercrowdedExeption {
-        String surname = JOptionPane.showInputDialog ("What is student name?");
+        String surname = JOptionPane.showInputDialog("What is student name?");
 
-        String name = JOptionPane.showInputDialog ("What is student name?");
+        String name = JOptionPane.showInputDialog("What is student name?");
 
-        String input = JOptionPane.showInputDialog ("How old are you?") ;
+        String input = JOptionPane.showInputDialog("How old are you?");
         int age = Integer.parseInt(input);
 
-        String sex = JOptionPane.showInputDialog ("What is student sex : men or woman?");
+        String sex = JOptionPane.showInputDialog("What is student sex : men or woman?");
 
-        String input2 = JOptionPane.showInputDialog ("What is student number record book?") ;
+        String input2 = JOptionPane.showInputDialog("What is student number record book?");
         int numberRecordBook = Integer.parseInt(input2);
 
-        String input3 = JOptionPane.showInputDialog ("What is student cours?") ;
+        String input3 = JOptionPane.showInputDialog("What is student cours?");
         int cours = Integer.parseInt(input3);
 
-        String faculty = JOptionPane.showInputDialog ("What is faculty?");
+        String faculty = JOptionPane.showInputDialog("What is faculty?");
 
-        Student interSyudent=new Student(surname,name,age,sex,numberRecordBook,cours,faculty);
+        Student interSyudent = new Student(surname, name, age, sex, numberRecordBook, cours, faculty);
         this.addStudent(interSyudent);
         System.out.println(Arrays.toString(group));
-       return ;
+        return;
     }
 }
 
